0d86cd27221957f1d555bc19ac4294530ba5b92e
Your Name <you@example.com>
2025-06-20 08:59:47 +0530

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineconfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/billing-vite",
+  base: process.env.VITE_BASH_PATH || "/billing-vite",
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
+  optimizeDeps: {
+    // example: include: ['some-package']
+  }
 });
